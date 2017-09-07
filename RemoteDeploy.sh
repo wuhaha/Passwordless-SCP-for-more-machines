fcf580d8ac427ef8c5ab9985bbd85147946280ba
Wuhaha <fengshuangg@gmail.com>
2017-09-06 21:29:51 -0700

    Correct typos

--- RemoteDeploy.sh
+++ RemoteDeploy.sh
@@ -4,6 +4,6 @@ ssh-keygen
 
 For (Raspberry=1, Raspberry<=250, Raspberry++)
 do
-SCP ~/.ssh/id_rsa.pub Pi@192.168.1.$Raspberry:/home/.ssh/ #input password once
-SSH Pi@192.168.1.$Paspberry 'cat /home/Pi/.ssh/id_rsa.pub >> /home/Pi/.ssh/authorized_users; rm /home/Pi/.ssh/id_rsa.pub' # input password the second time
+SCP ~/.ssh/id_rsa.pub Pi@192.168.1.$Raspberry:/home/pi/.ssh/ #input password once
+SSH Pi@192.168.1.$Paspberry 'cat /home/pi/.ssh/id_rsa.pub >> /home/Pi/.ssh/authorized_users; rm /home/pi/.ssh/id_rsa.pub' # input password the second time
 done
